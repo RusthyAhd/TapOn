6940513e25aeacc078945eac6519c6ef3d9201a2
RishafH <risaf0226@gmail.com>
2024-10-04 20:40:13 +0530

    backend connect my pc

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose'); //import mongoose library
 
-const connection =mongoose.createConnection('mongodb://127.0.0.1:27017/Tapon-DB').on('open',()=>{ //create "TapOnDB" database into localhost
+const connection =mongoose.createConnection('mongodb+srv://rusthy2001:rusthy.01122001@tapon-atlas.9qjax.mongodb.net/Tapon-DB').on('open',()=>{ //create "TapOnDB" database into localhost
     console.log("Mongodb Successfully Connected with TapOn-Backend"); //print this when not have any error
 }).on('error',()=>{
     console.log("Connection Error"); //monavahari prasnayaknam
