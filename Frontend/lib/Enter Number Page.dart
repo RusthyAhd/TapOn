7c9e5daf200dfbcfd6cf8fa8a5a040c2f7b9f48c
JMNaveeth <naveethkinniya2001@gmail.com>
2024-10-06 16:39:43 +0530

    add fg

--- Frontend/lib/Enter Number Page.dart
+++ Frontend/lib/Enter Number Page.dart
@@ -19,7 +19,7 @@ class _EnternumberState  extends State<Enternumber>{
 
   String? validatePhoneNumber(String? value) {
     if (value == null || value.isEmpty) {
-      return 'Please enter your phone number';
+      return 'Please fg enter your phone number';
     } else if (!RegExp(r'^\d{10}$').hasMatch(value)) {
       return 'Please enter a valid 10-digit phone number';
     }
