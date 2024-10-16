ef5e273f078b66fedc3489421c125c96fcf46c6d
MohamedRishaf <rishaf5317@gmail.com>
2024-10-16 13:28:25 +0530

    sp_login_back button

--- Frontend/lib/Service_Provider/SP_Login.dart
+++ Frontend/lib/Service_Provider/SP_Login.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:tap_on/Home%20page.dart';
 import 'package:tap_on/Service_Provider/SP_Dashboard.dart';
 import 'package:tap_on/Service_Provider/SP_Register.dart';
-
 
 class SP_Login extends StatelessWidget {
   const SP_Login({super.key});
@@ -12,16 +12,19 @@ class SP_Login extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('TapOn'),
-          backgroundColor: Colors.amber[700],
-          elevation: 0,
           leading: IconButton(
             icon: Icon(Icons.arrow_back, color: Colors.black),
             onPressed: () {
-              // Add back button functionality here
-              Navigator.pop(context);
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => HomePage()),
+              );
+              // Action when the button is pressed
             },
           ),
+          title: const Text('TapOn'),
+          backgroundColor: Colors.amber[700],
+          elevation: 0,
         ),
         body: Padding(
           padding: const EdgeInsets.all(20.0),
@@ -53,10 +56,8 @@ class SP_Login extends StatelessWidget {
               // Log in button
               ElevatedButton(
                 onPressed: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => SP_Dashboard()));
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => SP_Dashboard()));
                 },
                 style: ElevatedButton.styleFrom(
                   backgroundColor: Colors.amber[700],
@@ -87,11 +88,8 @@ class SP_Login extends StatelessWidget {
               // Create new account button
               OutlinedButton(
                 onPressed: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) =>
-                              SP_Register()));
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => SP_Register()));
                 },
                 style: OutlinedButton.styleFrom(
                   padding: EdgeInsets.symmetric(vertical: 15),
